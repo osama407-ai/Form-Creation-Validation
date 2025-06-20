4e4390f5c3feb04459f2987bfc00ccc6dea7f11e
Thapelo <folotithapelo@gmail.com>
2025-06-20 22:21:14 +0200

    Added data fetching submission files to directory

---
+++ fetch-data.js
@@ -0,0 +1,23 @@
+ const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+
+    const dataContainer = document.getElementById('api-data');
+
+    try {
+       
+        const response = await fetch(apiUrl);
+
+        const users = await response.json();
+
+        dataContainer.innerHTML = '';
+
+        const userListElement = document.createElement('ul');
+
+        users.forEach(user => {
+            
+            const listItem = document.createElement('li');
+            listItem.textContent = user.name; 
+            userListElement.appendChild(listItem); 
+        });
+
+        dataContainer.appendChild(userListElement);
+
